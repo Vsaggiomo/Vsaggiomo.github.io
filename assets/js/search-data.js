0939d0b723409ab08197bb5c2502f79fabe80ce3
Vsaggiomo <vsaggiomo@gmail.com>
2025-12-31 16:22:30 +0000

    Deploying to gh-pages from @ Vsaggiomo/vsaggiomo.github.io@b2f295f18df751183f27c78f0c36fe74470e5f4b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
@@ -536,7 +529,7 @@ ninja.data = [{
         title: 'YouTube',
         section: 'Socials',
         handler: () => {
-          window.open("https://youtube.com/@v_saggiomo", "_blank");
+          window.open("https://youtube.com/@@v_saggiomo", "_blank");
         },
       },{
       id: 'light-theme',
